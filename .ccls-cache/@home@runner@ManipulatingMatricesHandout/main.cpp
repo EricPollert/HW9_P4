d8be3658c7ca4a241663b56729bc68e95c316d8a
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-27 05:34:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@ManipulatingMatricesHandout/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COLUMNS);
+  
+  const int COL_NUM = 3;
+  const int ROW_NUM = 10;
+  int AirQuality_Index [ROW_NUM][COL_NUM] = { {50, 68, 58},
+                                              {58, 115, 78},
+                                              {62, 110, 81},
+                                              {66, 124, 110},
+                                              {64, 152, 121},
+                                              {46, 143, 90},
+                                              {46, 143, 90},
+                                              {25, 91, 91},
+                                              {27, 19, 59},
+                                              {17, 13, 27} };
+  
+}
+
+void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COLUMNS){
+
+
+
+}
