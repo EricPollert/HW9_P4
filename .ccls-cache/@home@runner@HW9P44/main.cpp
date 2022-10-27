2da1210605fda49e286d1b64334aa585e9dcf18d
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-27 06:32:28 +0000

    developed the for loops for the matrix index and couts for part 3 of handout

--- .ccls-cache/@home@runner@HW9P44/main.cpp
+++ .ccls-cache/@home@runner@HW9P44/main.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
+void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COL);
+int AirQuality_Index(int matrix [][3], int N_ROWS, int city_COL);
+
 int main() {
 
-void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COL);
-int AirQuality_Index(int matrix [10][3], int N_ROWS, int city_COL);
+  int city_COL;
+  const int N_COL = 3;
+  const int N_ROWS = 10;
+  int AirQuality_Index[N_ROWS][N_COL] = { {50, 68, 58},
+                                          {58, 115, 78},
+                                          {62, 110, 81},
+                                          {66, 124, 110},
+                                          {64, 152, 121},
+                                          {46, 143, 90},
+                                          {46, 143, 90},
+                                          {25, 91, 91},
+                                          {27, 19, 59},
+                                          {17, 13, 27} };
   
-  const int COL_NUM = 3;
-  const int ROW_NUM = 10;
-  int AirQuality_Index [ROW_NUM][COL_NUM] = { {50, 68, 58},
-                                              {58, 115, 78},
-                                              {62, 110, 81},
-                                              {66, 124, 110},
-                                              {64, 152, 121},
-                                              {46, 143, 90},
-                                              {46, 143, 90},
-                                              {25, 91, 91},
-                                              {27, 19, 59},
-                                              {17, 13, 27} };
+
+
+printMatrix_3col(AirQuality_Index, N_ROWS, N_COL);
+
   
 }
 
 
 
 
-
-
-
-void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COLUMNS){
-
+void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COL){
+cout << matrix[10][2];
 
 
 }
